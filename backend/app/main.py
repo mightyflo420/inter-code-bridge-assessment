172319a0320bc25ea432b54e84917f49586c334c
mightyflo420 <florian.frick@epicfusion.com>
2025-07-21 22:31:40 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
